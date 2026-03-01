691e12f9130a206dccae3c6e4e8e44e83278dacc
Tezahc <tezahc23@gmail.com>
2026-03-01 11:57:56 +0100

    format output en int et redéfinition des variable via symbols

--- optimizer/build_repartition.py
+++ optimizer/build_repartition.py
@@ -1,11 +1,12 @@
 from sympy import symbols, Eq, solve
-from sympy.abc import a, b, c, d
 
 """Side project de calcul de répartition de liste de construction entre plusieurs planètes.
 les équations représentent le total à produire (qty) tandis que les autres expriment le temps de construction dans le chantier spatial + le temps de vol pour ce vaisseau jusqu'à la planète mère.
 
 l'enjeu étant que si le trajet est trop long, la planète devrait être exclue de la production sans compromettre le système d'équations.
 """
+
+a, b, c, d = symbols("a b c d")
 # définition des équations de base
 qty = Eq(a + b + c + d, 100)
 eq1 = Eq(4*a, 30*b + 500)
@@ -19,4 +20,4 @@ equations = [qty, eq1, eq2, eq3]
 solutions = solve(equations)
 
 
-print(solutions)
+print({var: int(round(sol.evalf(), 0)) for var, sol in solutions.items()})
